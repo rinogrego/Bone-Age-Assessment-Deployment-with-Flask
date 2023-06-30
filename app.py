390173d101d4d2acca5d7c3d1bb73ff40a743902
rinogrego <rino.grego@gmail.com>
2023-06-30 16:04:51 +0700

    trying to push to railway

--- app.py
+++ app.py
@@ -169,4 +169,4 @@ def assess_api():
         return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
